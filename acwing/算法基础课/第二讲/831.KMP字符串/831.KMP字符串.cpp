0455c2d46eea8e60c2a4870cc0155218d0ee9368
bddk520 <bddk520@163.com>
2025-03-11 16:25:53 +0800

    回顾acwing基础课的831

---
+++ acwing/算法基础课/第二讲/831.KMP字符串/831.KMP字符串.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+using namespace std;
+
+const int N = 1e6 + 10;
+
+char s[N], p[N];
+int ne[N];
+// 注意不能是next[N], c库中有这个变量
+
+int main()
+{
+    int n, m;
+    cin >> n >> p + 1 >> m >> s + 1;
+    for (int i = 2, j = 0; i <= n; i++)
+    {
+        while (j && p[i] != p[j + 1])
+        {
+            j = ne[j];
+        }
+        if (p[i] == p[j + 1])
+            j++;
+        ne[i] = j;
+    }
+
+    for (int i = 1, j = 0; i <= m; i++)
+    {
+        while (j && s[i] != p[j + 1])
+        {
+            j = ne[j];
+        }
+        if (s[i] == p[j + 1])
+            j++;
+        if (j == n)
+        {
+            cout << i - n << " ";
+            j = ne[j];
+        }
+    }
+
+    return 0;
+}
