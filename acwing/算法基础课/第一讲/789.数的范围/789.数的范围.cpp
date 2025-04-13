22c43f4d8a1793e36dfe70315de638e5760b2492
bddk520 <bddk520@gmail.com>
2025-04-13 23:26:39 +0800

    回顾基础课的789

---
+++ acwing/算法基础课/第一讲/789.数的范围/789.数的范围.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<algorithm>
+#include<cstring>
+
+using namespace std;
+
+const int N = 1e5 + 10;
+
+int a[N];
+
+int main()
+{
+    int n,q;
+    cin >> n >> q;
+    
+    return 0;
+}
